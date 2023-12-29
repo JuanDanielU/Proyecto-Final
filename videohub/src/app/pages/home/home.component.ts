83398e405c35fdc1cbccfc2a6da109a0f11d2145
DevAsta <JDUM1221@GMAIL.COM>
2023-12-29 01:25:44 -0400

    signup frontend almost done

--- videohub/src/app/pages/home/home.component.ts
+++ videohub/src/app/pages/home/home.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import  { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatToolbarModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   title = 'videohub';
+
+  logOut(): void {
+    
+  }
 }
