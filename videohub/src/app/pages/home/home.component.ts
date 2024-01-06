48a52f5864979e718f2642065748cc93ca0da6c8
DevAsta <JDUM1221@GMAIL.COM>
2024-01-06 00:35:34 -0400

    optimizing

--- videohub/src/app/pages/home/home.component.ts
+++ videohub/src/app/pages/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, inject } from '@angular/core';
 import { Video } from "../../models/video";
 import { VideoService } from '../../core/services/video.service';
 import { CommonModule } from '@angular/common';
-import { UserService } from '../../core/services/user.service';
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -12,18 +12,17 @@ import { UserService } from '../../core/services/user.service';
 })
 export class HomeComponent {
   listVideos: Video[] = [];
-  private _userService = inject(UserService);
-  constructor(private _videoService: VideoService) {  }
+  private _videoService = inject(VideoService);
 
   ngOnInit() {
     this.getVideos();
   }
 
   getVideos() {
-  return this._videoService.getVideos().subscribe((data) => {
-    this.listVideos = data; // Asigna los datos a la lista de videos
-  }, (error) => {
-    console.log(error);
-  });
-}
+    return this._videoService.getVideos().subscribe((data) => {
+      this.listVideos = data; // Asigna los datos a la lista de videos
+    }, (error) => {
+      console.log(error);
+    });
+  }
 }
