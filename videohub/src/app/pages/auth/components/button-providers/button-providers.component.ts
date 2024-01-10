6c506fb79dee73b33a70c315ba26e49a0c4de12f
DevAsta <JDUM1221@GMAIL.COM>
2024-01-10 04:01:27 -0400

    refactorizacion, se elimino login, signup y majenar users con mongo db

--- videohub/src/app/pages/auth/components/button-providers/button-providers.component.ts
+++ videohub/src/app/pages/auth/components/button-providers/button-providers.component.ts
@@ -1,9 +1,6 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component, Input, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { User } from '../../../../models/user';
-
-import { UserService } from '../../../../core/services/user.service';
 import { AuthService } from '../../../../core/services/auth.service';
 
 export type Provider = 'google';
@@ -18,30 +15,13 @@ export type Provider = 'google';
 export class ButtonProviders {
   @Input() isLogin = false;
 
-  private _userService = inject(UserService);
   private _authService = inject(AuthService);
   private _router = inject(Router);
 
   async signUpWithGoogle(): Promise<void> {
     try {
-      const result = await this._authService.signInWithGoogleProvider();
-      const dbUser = await this._userService.getUser(result.user.uid).toPromise();
-      if (result.user.uid !== dbUser._id) {
-        const userData: User = {
-          _id: result.user.uid,
-          email: result.user.email!,
-          name: result.user.displayName!,
-          createdAt: new Date(result.user.metadata.creationTime!),
-          updatedAt: null,
-          photoURL: result.user.photoURL,
-        };
-        await this._userService.createUser(userData).toPromise();
-        this._router.navigateByUrl('/');
-      }
-      else
-      {
-        this._router.navigateByUrl('/');
-      }
+      await this._authService.signInWithGoogleProvider();
+      this._router.navigate(['/']);
     } catch (error) {
       console.log(error);
     }
