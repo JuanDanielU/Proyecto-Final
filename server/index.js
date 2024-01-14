ee608626ffd52808d54b4b4d16aa9bf1fb4099c0
DevAsta <JDUM1221@GMAIL.COM>
2024-01-14 14:49:19 -0400

    Coments

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use('/api/videos', require('./routes/video'));
-app.use('/api/users', require('./routes/user'));
+app.use('/api/videos', require('./routes/video')); // route for videos
+app.use('/api/users', require('./routes/user')); // route for users
 
 app.listen(4000, () => {
     console.log('Server running OK');
